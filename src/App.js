a02b8a734cf274949768cfa88db09531688032e2
Mani Ranjbaran <Ran_mani@yahoo.com>
2024-11-19 14:07:18 +0330

    made some changes

--- src/App.js
+++ src/App.js
@@ -25,6 +25,8 @@ export default function App() {
 
   function handleAddWatched(movie) {
     setWatched((watched) => [...watched, movie]);
+
+    // localStorage.setItem(JSON.stringify("watched", [...watched, movie]));
   }
 
   function handleRemoveMovie(id) {
